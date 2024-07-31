e76d685fdba9f3a9254f8e16f802f75fb98d1e2f
unknown <t043300520@unitru.edu.pe>
2024-07-31 11:57:08 -0500

    objectivos

--- src/dashboard/sidebar/data.tsx
+++ src/dashboard/sidebar/data.tsx
@@ -1,11 +1,7 @@
-import { DocIcon } from "./icons/DocIcon";
 import { HomeIcon } from "./icons/HomeIcon";
-import { TaskIcon } from "./icons/TaskIcon";
-import { ReportIcon } from "./icons/ReportIcon";
+
 import { ProjectIcon } from "./icons/ProjectIcon";
-import { SettingsIcon } from "./icons/SettingsIcon";
-import { CalendarIcon } from "./icons/CalendarIcon";
-import { TimeManageIcon } from "./icons/TimeManageIcon";
+
 
 export const data = [
   {
